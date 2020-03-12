6aa3e1258c77231ccdaf8d774073d88a90303afc
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-03-11 22:00:15 -0500

    Update module 8 problem 3.py

--- module 8 problem 3.py
+++ module 8 problem 3.py
@@ -5,15 +5,29 @@
 def value(lst):
     for i in lst:
         if i == 5:
+            #need a print statement here instead
+            #print ("Five is in the list")
          result = "5 is in the list"
 
     return result
-
+#why two lists?
 lst = [1,2,3,4,5]
 lst2 = [2,3]
 
 print(value(lst))
 print(value(lst2))
+
+#def value(lst):
+#    for i in lst:
+#       if   i == 5:
+#         print ("5 is in the list")
+#         return
+
+#lst = [1,2,3,4,5]
+
+#value(lst)
+
+
 
 
 
